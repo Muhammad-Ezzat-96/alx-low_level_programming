729e9b36e05da5aab8d11bace87f41a15f161eaa
Muhammad-Ezzat-96 <eng.muhammad.ezzat.22@gmail.com>
2023-09-09 19:39:19 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+  *main - Entry point
+  *Discription: all base 10 single digit numbers
+  *Return: always 0
+  */
+int main(void)
+{
+	int digit = 0;
+
+	for (digit = 0; digit < 10; digit++)
+	{
+		printf("%d\n", digit);
+	}
+	return (0);
+}
