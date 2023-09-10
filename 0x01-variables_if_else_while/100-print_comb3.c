bd78b59ac9af20c03d036c48de411cc2a789926f
Muhammad-Ezzat-96 <eng.muhammad.ezzat.22@gmail.com>
2023-09-10 01:50:51 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+  *main - Entry point
+  *Discription: a program that prints all possible different
+  *combinations of two digits
+  *Return: always 0
+  */
+int main(void)
+{
+	int x = '0';
+	int y = '0';
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		for (y = '0'; y <= '9'; y++)
+		{
+			if (x != y && x < y)
+			{
+				putchar(x);
+				putchar(y);
+
+				if (x + y != 17)
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
