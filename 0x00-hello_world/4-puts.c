6bf75477d037ad7e4594c4092b8d7539f617a18e
Muhammad-Ezzat-96 <eng.muhammad.ezzat.22@gmail.com>
2023-09-11 18:16:14 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
+#include <string.h>
 /**
-*main - entry poit
-*return: always 0 (success)0
-*/
+  *main - Entry point
+  *Discription:a C program that prints
+  *Programming is like building a multilingual puzzle
+  *Return: always 0
+  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle);
+	char str1[52];
+
+	strcpy(str1, "\"Programming is like building a multilingual puzzle");
+
+	puts(str1);
+
 	return (0);
 }
