fd62cd58fc3af22f7e777e09774358bc4d041fd9
Muhammad-Ezzat-96 <eng.muhammad.ezzat.22@gmail.com>
2023-09-15 00:36:42 -0700

    5-main.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,15 +6,17 @@
   */
 void more_numbers(void)
 {
-	int x = 0;
-	int y;
+	int x, y;
 
 	for (x = 0; x < 10; x++)
 	{
 		for (y = 0; y <= 14; y++)
 		{
-			_putchar(y / 10 + '0');
-			_putchar(y % 10 + '0');
+			if (y >= 10)
+			{
+				_putchar(y / 10 + 48);
+				_putchar(y % 10 + 48);
+			}
 		}
 		_putchar('\n');
 	}
