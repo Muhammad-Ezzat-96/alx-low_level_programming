24f99c112158e933ce3dce69ddbabbe36f9086dc
Muhammad-Ezzat-96 <eng.muhammad.ezzat.22@gmail.com>
2023-09-14 20:29:16 -0700

    2-main.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  *mul - functiond that multiplies
+  *Discription:
+  *Return: always 0
+  */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
