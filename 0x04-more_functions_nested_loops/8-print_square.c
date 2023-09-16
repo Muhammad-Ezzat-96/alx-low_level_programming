82557b5ad981483462631e4fecd1480835dccdd8
Muhammad-Ezzat-96 <eng.muhammad.ezzat.22@gmail.com>
2023-09-15 18:57:29 -0700

    8-main.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+  *print_square - referes to the size of the square
+  *@size : size of square
+  *Return: always 0
+  */
+void print_square(int size)
+{
+	int x, y;
+
+	y = 0;
+
+	if (size < 1)
+		_putchar('\n');
+
+	while (y < size)
+	{
+		x = 0;
+		while (x < size)
+		{
+			_putchar('#');
+			x++;
+		}
+		_putchar('\n');
+		y++;
+	}
+}
