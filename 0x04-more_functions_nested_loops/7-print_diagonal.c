76aa752ab4411a92599341281903bec6b7693b44
Muhammad-Ezzat-96 <eng.muhammad.ezzat.22@gmail.com>
2023-09-15 18:37:28 -0700

    7-main.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  *print_diagonal - prints a diagonal
+  *@n : a charchter that specifies the shape of the diagonal
+  *Return: always 0
+  */
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+				{
+					_putchar('\\');
+				}
+				else if (j < i)
+				{
+					_putchar(' ');
+				}
+			}
+		}
+		_putchar('\n');
+	}
+}
