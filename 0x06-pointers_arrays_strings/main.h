ea07e73c9feab7eb4564e109f3952536aa1af38d
Muhammad-Ezzat-96 <eng.muhammad.ezzat.22@gmail.com>
2023-09-21 19:13:53 -0700

    7-main.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <string.h>
 
+char *leet(char *);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
