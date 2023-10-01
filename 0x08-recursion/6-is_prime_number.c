04813ffd241fff584d6c51c24ea6c0053e79e4f8
Muhammad-Ezzat-96 <eng.muhammad.ezzat.22@gmail.com>
2023-10-01 06:29:37 -0700

    6-main.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+  *sup_fun - auciliary function
+  *@n : input value
+  *@i : the factor value
+  *@count : the count of factors
+  *Return: either 1 or 0
+  */
+int sup_fun(int n, int i)
+{
+	if (n == i)
+	{
+		return (1);
+	}
+	else if ( n % i == 0)
+	{
+		return (0);
+	}
+	else
+	{
+	return (sup_fun(n, i + 1));
+	}
+}
+/**
+  *is_prime_number - checks for a number whether prime or not
+  *@n : input value
+  *Return: always 1 if prime, 0 if not
+  */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	return (sup_fun(n, 2));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,5 +10,6 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int _putchar(char c);
 int aux_fun(int sqrt ,int rt);
+int sup_fun(int n, int i);
 
 #endif
