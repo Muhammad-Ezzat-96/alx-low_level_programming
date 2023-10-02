75ad5c5dc79547e2a1dcd3a720e88318fa6376fb
Muhammad-Ezzat-96 <eng.muhammad.ezzat.22@gmail.com>
2023-10-02 15:21:05 -0700

    main.h

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+  *main - Entry point
+  *@argc: counts the # of arguments
+  *@argv: the content ofthe arguments
+  *Return: always 0
+  */
+int main(int argc, char* argv[])
+{
+	int i = 0;
+	(void)argv;
+
+	while (i < argc)
+	{
+		i++;
+	}
+	printf("%d\n", i - 1);
+	return (0);
+}
+
