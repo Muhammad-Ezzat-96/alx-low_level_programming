e8a1b8e4fb3274ff25a5cae591f5863d2dc0a421
Muhammad-Ezzat-96 <eng.muhammad.ezzat.22@gmail.com>
2023-10-03 09:44:43 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -7,51 +7,39 @@
   *Return: always 0
   */
 int main(int argc, char *argv[])
-{
-	int i = 0;
-	int j;
+{int i = 0, j;
 
 	if (argc == 2)
 	{
 		j = atoi(argv[1]);
-		if (j < 0)
-		{
-			printf("0\n");
-		}
 		while (j > 0)
 		{
 			if (j % 25 < j)
-			{
-				j = j - 25;
+			{j = j - 25;
 				i++;
 			}
 			else if (j % 10 < j)
-			{
-				j = j - 10;
+			{j = j - 10;
 				i++;
 			}
 			else if (j % 5 < j)
-			{
-				j = j - 5;
+			{j = j - 5;
 				i++;
 			}
 			else if (j % 2 < j)
-			{
-				j = j - 2;
+			{j = j - 2;
 				i++;
 			}
 			else if (j % 1 < j)
-			{
-				j = j - 1;
+			{j = j - 1;
 				i++;
 			}
 		}
 		printf("%d\n", i);
 	}
 	else
-	{
-		printf("Error\n");
-		return (1);
+	{printf("Error\n");
+	return (1);
 	}
 	return (0);
 }
