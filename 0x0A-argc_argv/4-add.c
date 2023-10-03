7c0dbf9b71fe795f7cc9182d49ff92c612be606c
Muhammad-Ezzat-96 <eng.muhammad.ezzat.22@gmail.com>
2023-10-03 01:13:50 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  *main - Entry point
+  *@argc: number of arguments
+  *@argv: content of argument
+  *Return: always 0
+  */
+int main(int argc, char *argv[])
+{
+	int i;
+	int j;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	else if (argc > 1)
+	{
+		j = atoi(argv[0]);
+		for (i = 1; i < argc; i++)
+		{
+			if (*argv[i] < 48 || *argv[i] > 57)
+			{
+				printf("Error\n");
+				return (1);
+			}
+			j = j + (atoi(argv[i]));
+		}
+		printf("%d\n", j);
+	}
+	return (0);
+}
