a4cd22762d98b8c34ee99b4375fa300adefaf3e0
Muhammad-Ezzat-96 <eng.muhammad.ezzat.22@gmail.com>
2023-10-08 11:47:59 -0700

    0-main.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  *malloc_checked - function that automatically allocates a memory
+  *@b: input value
+  *Return: either a pointer to the allocated memory or 98 if it fails
+  */
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *ptr;
+
+	ptr = malloc(sizeof(unsigned int) * b);
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
