f4c46f6d11dfff817c9c5ae041ab20f76e2e9400
Muhammad-Ezzat-96 <eng.muhammad.ezzat.22@gmail.com>
2023-10-11 16:27:19 -0700

    0-main.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  *print_name - prints a name
+  *@name: the name that that is determined at the main function
+  *@f: pointer to a function
+  */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
