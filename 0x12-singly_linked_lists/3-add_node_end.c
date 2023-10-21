a45432be8e4f1fd5f5c8cb1b906ff332f84e853e
Muhammad-Ezzat-96 <eng.muhammad.ezzat.22@gmail.com>
2023-10-20 19:40:17 -0700

    3-main.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,7 +11,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	char *dup;
 	int i = 0;
 
-	node = (list_t*)malloc(sizeof(list_t));
+	node = (list_t *)malloc(sizeof(list_t));
 	if (node == NULL)
 		return (NULL);
 	dup = strdup(str);
@@ -40,5 +40,5 @@ list_t *add_node_end(list_t **head, const char *str)
 	node->str = dup;
 	node->len = i;
 	node->next = NULL;
-	return (node->next);
+	return (*head);
 }
