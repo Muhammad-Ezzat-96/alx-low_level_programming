4debde87c11c14712feeb8b77b94e09491f844cc
Muhammad-Ezzat-96 <eng.muhammad.ezzat.22@gmail.com>
2023-10-22 16:07:08 -0700

    0-main.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+  *_function - prints a given string before the main function
+  *Return: nothing
+  */
+void  __attribute__((constructor)) _function(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
