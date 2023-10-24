8c670b5f358e392ca942fb5d733e133afe8fa4c5
Muhammad-Ezzat-96 <eng.muhammad.ezzat.22@gmail.com>
2023-10-23 20:28:06 -0700

    4-main.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+  *free_listint - frees a listint_t list
+  *@head: the head node
+  *Return: nothing
+  */
+void free_listint(listint_t *head)
+{
+	listint_t *clear = head->next;
+
+	while (head != NULL)
+	{
+		free(head);
+		clear = clear->next;
+		head = clear;
+	}
+}
