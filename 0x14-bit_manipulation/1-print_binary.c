4cee3306bdb16eb16138d1d2c8b019f62e9a689c
Muhammad-Ezzat-96 <eng.muhammad.ezzat.22@gmail.com>
2023-10-26 19:24:49 -0700

    main.h

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,22 +6,8 @@
   */
 void print_binary(unsigned long int n)
 {
-	unsigned long int i = 1ul << 63;
-	char c = '0';
+	if (n > 1)
+		print_binary(n >> 1);
 
-	while (!(i & n) && i != 0)
-		i = i >> 1;
-
-	if (i == 0)
-		write(1, &c, 1);
-
-	while (i)
-	{
-		if (i & n)
-			c = '1';
-		else
-			c = '0';
-		write(1, &c, 1);
-		i = i >> 1;
-	}
+	_putchar((n & 1) + '0');
 }
