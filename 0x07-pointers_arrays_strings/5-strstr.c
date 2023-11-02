4ecf5f6dc20ffd8e1b4ba5ad55fbd6077e15c770
Muhammad-Ezzat-96 <eng.muhammad.ezzat.22@gmail.com>
2023-11-02 07:55:38 -0700

    5-main.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,27 +1,33 @@
 #include "main.h"
 /**
-  *_strstr - function that locates a substring
-  *@haystack: the full string
-  *@needle: the substring
-  *Return: pointer to the beginning of the located substring, or NULL
-  */
+ * _strstr - checks if needle starts with haystack
+ * @haystack: string to search
+ * @needle: the substring to find
+ *
+ * Return: address of next char of haystack or NULL
+ */
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j;
+	int i;
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
 		if (haystack[i] == needle[0])
 		{
+			int j;
+
 			for (j = 0; needle[j] != '\0'; j++)
 			{
-				if (needle[j] != haystack[i + j])
+				if (haystack[i + j] != needle[j])
+				{
 					break;
+				}
 			}
+
 			if (needle[j] == '\0')
+			{
 				return (&haystack[i]);
-			else
-				return (NULL);
+			}
 		}
 	}
 	return (NULL);
