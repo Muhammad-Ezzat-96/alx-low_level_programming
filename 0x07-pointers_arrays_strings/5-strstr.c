cad5ede403a474aadb9335b7062d36a2da974e38
Muhammad-Ezzat-96 <eng.muhammad.ezzat.22@gmail.com>
2023-11-02 06:45:31 -0700

    main.h

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -16,9 +16,10 @@ char *_strstr(char *haystack, char *needle)
 			for (j = 0; needle[j] != '\0'; j++)
 			{
 				if (needle[j] != haystack[i + j])
-					break;
+					return (NULL);
 			}
-		return (&haystack[i]);
+			if (needle[j] == '\0')
+				return (&haystack[i]);
 		}
 	}
 	return (NULL);
