6b22b65262971b4e50868666527cb86b19f72104
Muhammad-Ezzat-96 <eng.muhammad.ezzat.22@gmail.com>
2023-11-02 06:40:58 -0700

    main.h

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+  *_strstr - function that locates a substring
+  *@haystack: the full string
+  *@needle: the substring
+  *Return: pointer to the beginning of the located substring, or NULL
+  */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] == needle[0])
+		{
+			for (j = 0; needle[j] != '\0'; j++)
+			{
+				if (needle[j] != haystack[i + j])
+					return (NULL);
+			}
+		return (&haystack[i]);
+		}
+	}
+	return (NULL);
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+#ifndef main_h
+#define main_h
+
+#include <stdio.h>
+
+char *_strstr(char *haystack, char *needle);
+
+#endif
