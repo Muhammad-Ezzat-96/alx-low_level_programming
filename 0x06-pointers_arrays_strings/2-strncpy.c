a0e4e9a4eff7eb120e7c5da743a2414edfe7bc1c
Muhammad-Ezzat-96 <eng.muhammad.ezzat.22@gmail.com>
2023-11-03 05:50:16 -0700

    main.h

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,12 +10,15 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	while (src[i] != '\0')
+	while (src[i] != '\0' && i < n)
 	{
 		dest[i] = src[i];
 		i++;
-		if (i > n)
-			break;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
 	}
 	return (dest);
 }
