a0f00456058c450ffbdd51315e58f7bdc3ebe523
Muhammad-Ezzat-96 <eng.muhammad.ezzat.22@gmail.com>
2023-11-19 19:30:12 -0800

    main.h

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -3,8 +3,8 @@
   *_memcpy - function that copies memory area
   *@dest: the copied bites
   *@src: the bites to be copied
-  *@n: the number of bites that needs to be copied
-  *Return: pointer t dest
+  *@n: the number of bites to be copied
+  *Return : pointer to dest
   */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
